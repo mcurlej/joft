d7982b7f2257f8c0f56442f091c9e4affe2e0a36
Martin Curlej <mcurlej@redhat.com>
2025-06-07 14:45:12 +0200

    Added feature which enables you to use config as a commmand line option.
    Signed-off-by: Martin Curlej <mcurlej@redhat.com>

--- joft/utils.py
+++ joft/utils.py
@@ -28,8 +28,29 @@ def read_and_validate_config(
     return config
 
 
-def load_toml_app_config() -> typing.Any:
-    possible_paths = []
+def load_toml_app_config(config_path: str | None = None) -> typing.Any:
+    """Loads the TOML application configuration.
+
+    If a config_path is provided, it will be loaded first. Otherwise, it will search for the config file in the default locations.
+
+    Args:
+        config_path: Optional path to the config file.
+
+    Returns:
+        The application configuration as a dictionary.
+
+    Raises:
+        SystemExit: If the configuration file is invalid or not found.
+    """
+    possible_paths: list[str] = []
+
+    if config_path:
+        config_file_path = pathlib.Path(config_path)
+        try:
+            config = read_and_validate_config(config_file_path)
+            return config
+        except Exception as e:
+            _display_error_and_exit(config_file_path, possible_paths, e)
 
     possible_paths.append(str(pathlib.Path.cwd()))
     possible_paths.append(platformdirs.user_config_dir())
@@ -42,37 +63,34 @@ def load_toml_app_config() -> typing.Any:
             try:
                 config = read_and_validate_config(config_file_path)
             except Exception as e:
-                err_msg = textwrap.dedent(f"""\
-                    [ERROR] Configuration file {config_file_path} is invalid:
-
-                    {type(e).__name__} - {str(e)}
-
-                    Configuration file should have the following content:
-
-                    [jira.server]
-                    hostname = "<your jira server url>"
-                    pat_token = "<your jira pat token>"
-
-                    and should be stored in one of the following directories:
-                    {", ".join(possible_paths)}\
-                """)
-                print(err_msg)
-                sys.exit(1)
+                _display_error_and_exit(config_file_path, possible_paths, e)
             else:
                 return config
     else:
-        err_msg = textwrap.dedent(f"""\
-            [ERROR] Cannot find configuration file 'joft.config.toml'.
+        _display_error_and_exit(
+            config_file_path,
+            possible_paths,
+            FileNotFoundError("Configuration file not found"),
+        )
 
-            Create the file with the following content:
 
-            [jira.server]
-            hostname = "<your jira server url>"
-            pat_token = "<your jira pat token>"
+def _display_error_and_exit(
+    config_file_path: pathlib.Path, possible_paths: list[str], e: Exception
+) -> None:
+    err_msg = textwrap.dedent(f"""\
+        [ERROR] Configuration file {config_file_path} is invalid:
 
-            in one of the following directories:
-            {", ".join(possible_paths)}\
-        """)
+        {type(e).__name__} - {str(e)}
 
-        print(err_msg)
-        sys.exit(1)
+        Configuration file should have the following content:
+
+        [jira.server]
+        hostname = "<your jira server url>"
+        pat_token = "<your jira pat token>"
+
+        and should be stored in one of the following directories:
+        {", ".join(possible_paths)}\
+    """)
+
+    print(err_msg)
+    sys.exit(1)
